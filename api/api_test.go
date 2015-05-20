05ccd11b03faa934a22160aadac2e6c44e4dfdf4
Evan Hazlett <ejhazlett@gmail.com>
2015-05-20 02:22:28 -0400

    api: add initial test framework
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- api/api_test.go
+++ api/api_test.go
@@ -1,1 +1,64 @@
 package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/ehazlett/interlock"
+	"github.com/ehazlett/interlock/plugins"
+	"github.com/samalba/dockerclient"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockManager struct{}
+
+func (m MockManager) Run() error {
+	return nil
+}
+
+func (m MockManager) Stop() error {
+	return nil
+}
+
+func (m MockManager) Plugins() map[string]*plugins.RegisteredPlugin {
+	return nil
+}
+
+func (m MockManager) Config() *interlock.Config {
+	return nil
+}
+
+func (m MockManager) Client() *dockerclient.DockerClient {
+	return nil
+}
+
+func getTestApi() (*Api, error) {
+	log.SetLevel(log.ErrorLevel)
+	m := &MockManager{}
+
+	cfg := &ApiConfig{
+		ListenAddr: ":8080",
+		Manager:    m,
+	}
+
+	return NewApi(cfg), nil
+}
+
+func TestApiGetRoot(t *testing.T) {
+	api, err := getTestApi()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(api.apiIndex))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, res.StatusCode, http.StatusOK, "expected response code 200")
+}
