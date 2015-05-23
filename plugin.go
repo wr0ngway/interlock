a3c22971c0a1a01d4e597c662d0f3d6488d53a8c
Evan Hazlett <ejhazlett@gmail.com>
2015-05-23 13:35:02 -0400

    api: start on plugin actions
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- plugin.go
+++ plugin.go
@@ -4,11 +4,18 @@ import (
 	"github.com/samalba/dockerclient"
 )
 
+type PluginAction struct {
+	Name       string   `json:"name,omitempty"`
+	EventName  string   `json:"event_name,omitempty"`
+	Parameters []string `json:"parameters,omitempty"`
+}
+
 type PluginInfo struct {
-	Name        string `json:"name,omitempty"`
-	Version     string `json:"version,omitempty"`
-	Description string `json:"description,omitempty"`
-	Url         string `json:"url,omitempty"`
+	Name        string          `json:"name,omitempty"`
+	Version     string          `json:"version,omitempty"`
+	Description string          `json:"description,omitempty"`
+	Url         string          `json:"url,omitempty"`
+	Actions     []*PluginAction `json:"actions,omitempty"`
 }
 
 type Plugin interface {
